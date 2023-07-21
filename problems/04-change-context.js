98467e81cc1203569020a08f33de94f5b24d0350
StephanyKleen <stephanykleen@gmail.com>
2023-07-21 10:49:52 -0700

    Phase 4: Use arrow functions to keep the context when utlizing functions

--- problems/04-change-context.js
+++ problems/04-change-context.js
@@ -1,6 +1,22 @@
-function changeContext(func, obj) {
-  // Your code here
+
+
+const changeContext = (func, obj) => func.call(obj);
+
+
+/* FOR TESTING ONLY
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
 }
+
+function extractName() {
+  return this.name;
+}
+
+const kristen = new Person('Kristen');
+console.log(changeContext(extractName, kristen)); // => Kristen
+*/
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
