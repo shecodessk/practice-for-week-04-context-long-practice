916bd588dec7c6dc3f279ee1756b69db6b56b486
StephanyKleen <stephanykleen@gmail.com>
2023-07-21 12:37:40 -0700

    Using parent class to access calculator properties. Create new methods to modify the total instance property value

--- problems/06-fancy-calculator.js
+++ problems/06-fancy-calculator.js
@@ -1,4 +1,39 @@
-// Your code here
+	
+	const Calculator = require('./02-calculator')
+
+	class FancyCalculator extends Calculator{
+		constructor(total){
+			super(total)
+		}
+
+		setTotal(newTotal){
+			this.total = newTotal;
+			return this.total;
+		}
+
+		modulo(num){
+			let remainder = this.total % num;
+			return remainder;
+		}
+
+		squared(){
+			let squaredTotal = this.total * this.total;
+			return squaredTotal; 
+		}
+	}
+
+/* FOR TESTING ONLY
+let fancyCalculator = new FancyCalculator();
+console.log(fancyCalculator.setTotal(5)); // => 5
+console.log(fancyCalculator.squared());   // => 25
+console.log(fancyCalculator.modulo(4));   // => 1
+console.log(fancyCalculator.total)        // => 1
+
+// can use instance methods on the Calculator class
+console.log(fancyCalculator.add(9))  
+*/
+
+
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
