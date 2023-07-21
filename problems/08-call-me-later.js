2c8b0a8b5f942647ba8cd6929257d8dd0db04dd1
StephanyKleen <stephanykleen@gmail.com>
2023-07-21 13:25:40 -0700

    Use arrow fat functions to bind context without utilizing bind, call or apply methods

--- problems/08-call-me-later.js
+++ problems/08-call-me-later.js
@@ -1,5 +1,26 @@
-// Your code here
 
+class CallCenter{
+	constructor(name){
+		this.name = name;
+	}
+
+	sayHello = () => console.log(`Hello this is ${this.name}`);
+
+	callMeLater (delay){ 
+		setTimeout(this.sayHello, delay);
+	}
+}
+
+/* FOR TESTING ONLY
+let judy = new CallCenter("Judy");
+judy.sayHello();         // prints "Hello this is Judy"
+judy.callMeLater(1000);  // waits one second then prints "Hello this is Judy"
+
+
+let melan = new CallCenter("Melan");
+melan.sayHello();        // prints "Hello this is Melan"
+melan.callMeLater(1000); // waits one second then prints "Hello this is Melan"
+*/
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
