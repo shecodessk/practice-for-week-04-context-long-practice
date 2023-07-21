224cc25eb146eb20a8a8d8741e7fcf8a3321bda6
StephanyKleen <stephanykleen@gmail.com>
2023-07-21 14:16:52 -0700

    Passing incoming arguments to the class property array through instances

--- problems/10-party-planner.js
+++ problems/10-party-planner.js
@@ -1,5 +1,32 @@
-// Your code here
+class PartyPlanner{
+constructor(){
+	this.guestList = [];
+}
 
+addToGuestList(name){
+	this.guestList.push(name);
+}
+
+throwParty(){
+	if(this.guestList.length <= 0){
+		return `Gotta add people to the guest list`
+	}
+
+	let allNames = this.guestList.join(" and ")
+	return `Welcome to the party ${allNames}`
+	}
+}
+
+
+const party = new PartyPlanner();
+
+console.log(party.throwParty()); // prints "Gotta add people to the guest list"
+
+party.addToGuestList("James");
+console.log(party.throwParty()); // prints "Welcome to the party James"
+
+party.addToGuestList("Alvin");
+console.log(party.throwParty()); // prints "Welcome to the party James and Alvin"
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
