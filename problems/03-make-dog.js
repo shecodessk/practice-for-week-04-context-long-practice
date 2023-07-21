54f34fd564c71ae758877806bc6bcbb6c79b2a05
StephanyKleen <stephanykleen@gmail.com>
2023-07-21 10:27:17 -0700

    Phase 3: Solving Dog Class with static and instant methods

--- problems/03-make-dog.js
+++ problems/03-make-dog.js
@@ -1,5 +1,35 @@
 // Your code here
+class Dog{
+	constructor(name){
+		this.name = name;
+	}
 
+	static makeJet(){
+		 let dog = new Dog('Jet');
+		 return dog
+	}
+
+	changeName(newName){
+		this.name = newName;
+		return this.name 
+	}
+
+	speak(word){
+		return `${this.name} says ${word}`
+	}
+}
+/* FOR TESTING ONLY
+let dog1 = Dog.makeJet(); // returns an object
+
+console.log(dog1.name);                 // Jet
+console.log(dog1.speak("hello"));       // Jet says hello
+console.log(dog1.changeName("Freyja")); // Freyja
+console.log(dog1.name);                 // Freyja
+console.log(dog1.speak("hello"));       // Freyja says hello
+
+let dog2 = Dog.makeJet();
+console.log(dog2.name);  
+*/
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
