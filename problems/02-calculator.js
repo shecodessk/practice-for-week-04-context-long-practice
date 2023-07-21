48c1555e024e465841fd905af7e74bd3e97de6bb
StephanyKleen <stephanykleen@gmail.com>
2023-07-21 10:13:56 -0700

    Solving the Calculator problem creating methods that alter the initialized total property

--- problems/02-calculator.js
+++ problems/02-calculator.js
@@ -1,5 +1,41 @@
 // Your code here
+class Calculator{
+ 	constructor(){
+		this.total = 0;
+	}
 
+	add(num){
+		let total = this.total + num;
+		this.total = total;
+		return this.total;
+	}
+
+	subtract(num){
+		let total = this.total - num;
+		this.total = total;
+		return this.total
+	}
+	divide(num){
+		let total = this.total / num;
+		this.total = total;
+		return this.total;
+	}
+
+	multiply(num){
+		let total = this.total * num;
+		this.total = total;
+		return this.total;
+	}
+}
+
+/* FOR TESTING ONLY
+let calculator = new Calculator();
+console.log(calculator.add(50));      // => 50
+console.log(calculator.subtract(35)); // => 15
+console.log(calculator.multiply(10)); // => 150
+console.log(calculator.divide(5));    // => 30
+console.log(calculator.total)         // => 30
+*/
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
