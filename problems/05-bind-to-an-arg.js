ca297e6377f4b26342a9d65ad0e240d8b737b292
StephanyKleen <stephanykleen@gmail.com>
2023-07-21 11:51:25 -0700

    Bound arguments to a specific context in regular functions

--- problems/05-bind-to-an-arg.js
+++ problems/05-bind-to-an-arg.js
@@ -1,6 +1,41 @@
-function bindToAnArg(func, arg) {
-  // Your code here
+//You need to pass this, as the binding method will expect context as its first argument. Because we want bindToAnArg to always bind to the arg passed, this should always refer to bindToAnArg.
+
+const bindToAnArg = (func, arg) => func.bind(this, arg)
+
+
+
+/* FOR TESTING ONLY
+function add(num1, num2) {
+  return num1 + num2;
 }
+
+const addTwo = bindToAnArg(add, 2);
+const addThree = bindToAnArg(add, 3);
+
+const twoPlusSix = addTwo(6);
+const twoPlusSeven = addTwo(7);
+const threePlusSeven = addThree(7);
+const threePlusEight = addThree(8);
+console.log({
+  twoPlusSix,     // => 8
+  twoPlusSeven,   // => 9
+  threePlusSeven, // => 10
+  threePlusEight  // => 11
+});
+
+function iSpy(thing) {
+  return "I spy a " + thing;
+}
+
+let spyTree = bindToAnArg(iSpy, "tree");
+console.log(spyTree());        // => I spy a tree
+console.log(spyTree("car"));   // => I spy a tree
+
+let spyCar = bindToAnArg(iSpy, "car");
+console.log(spyCar());         // => I spy a car
+console.log(spyCar("potato")); // => I spy a car
+*/
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = bindToAnArg;
